e0ff8b311ca599f87d57e49a26b66d59e4b5676e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-25 20:06:15 +0000

    Deploy azatmagdeev/avtomarket to github.com/azatmagdeev/avtomarket.git:gh-pages

--- js/app.js
+++ js/app.js
@@ -3,18 +3,30 @@ import {Ads} from "./lib.js";
 const ads = new Ads();
 
 class App {
-    constructor(rootEl, ads) {
+    constructor(rootEl, ads, addBtn) {
         this.rootEl = rootEl;
         this.ads = ads;
-        this.init()
+        this.init();
+        this.addBtn = addBtn;
+        this.addBtn.addEventListener('click', () => {
+            this.addNewAd()
+        });
+
     }
 
     init() {
+        // todo: parse current url
 
-      this.rootEl.innerHTML = `    <div class="mt-3">
+        this.viewLastAds()
+
+
+    }
+
+    viewLastAds(){
+        this.rootEl.innerHTML = `    <div class="mt-3">
                     <b><h4>Новые объявления:</h4> </b></div>
                 <div class="card-deck" id=""> </div>`;
-      const cardDeck = document.querySelector('.card-deck');
+        const cardDeck = document.querySelector('.card-deck');
 
         this.ads.getItems(items => {
             items.reverse().map(o => {
@@ -27,7 +39,7 @@ class App {
                             <a href="#" class=""><h5 class="card-title">${o.brand} ${o.model}, ${o.year}</h5></a>
                             <p class="card-text"><b>${o.price} руб.</b></p>
                             <p class="card-text">Казань</p>
-                            <p class="card-text">${o.date}</p>
+<!--                            <p class="card-text">${o.date}</p>-->
 
                         </div>
                     </div>
@@ -39,21 +51,22 @@ class App {
                         this.viewItem(o);
                     })
 
+
                 }
             )
         });
     }
 
     viewItem(o) {
-        console.log(
-           formateDate(o.date)
-            );
-
-        this.ads.getSellers(sellers=>{
-            console.log('sellers',sellers);
+        // history.pushState(null, 'детали', '/details.html');
+        // console.log(
+        //     formateDate(o.date)
+        // );
+        this.ads.getSellers(sellers => {
+            console.log('sellers', sellers);
             const seller = sellers.filter(seller => seller.id === o.sellerId)[0];
-            console.log('seller',seller);
-            this.rootEl.textContent ='';
+            console.log('seller', seller);
+            this.rootEl.textContent = '';
             this.rootEl.innerHTML = `
             <button class="btn btn-primary mt-3" id="backBtn"><- Назад</button>
             <h3 class="">${o.brand} ${o.model}, ${o.year}</h3>
@@ -68,20 +81,44 @@ class App {
             <h3 class="mt-3" style="color: blue">Цена: <b>${o.price} руб.</b></h3>
             <p>${o.text}</p>
             <p class="mt-3"><h5>Продавец: ${seller.name}</h5></p>
-            <button class="btn btn-primary" id="callSeller">Позвонить продавцу</button>
+            <button class="btn btn-primary" id="showPhone">Показать телефон</button>
             <button class="btn btn-outline-secondary" id="smsSeller">Написать сообщение</button>
             <p class="mt-3">${formateDate(o.date)}</p>
             `;
             const backBtn = document.getElementById('backBtn');
-            backBtn.addEventListener('click',()=>{this.init()})
+            backBtn.addEventListener('click', () => {
+                this.init()
+            });
+            const showPhone = document.getElementById('showPhone');
+            showPhone.addEventListener('click',()=>{
+                showPhone.innerHTML = `<b>${seller.phoneNumber}</b>`;
+                showPhone.className = 'btn btn-outline-secondary';
+            })
         });
 
     }
+
+    addNewAd() {
+        console.log('button works!');
+        this.rootEl.innerHTML = `
+        <form class='form'>
+        <select name="brand" id="brand">
+            <option value="Lada">Lada</option>
+            <option value="Renault">Renault</option>
+            <option value="Mazda">Mazda</option>
+        </select>
+        <select name="model" id="model">
+        
+        </select>
+        <input type="text">
+        </form>
+        `;
+    }
 }
 
-const app = new App(document.getElementById('root'), ads);
+const app = new App(document.getElementById('root'), ads, document.getElementById('addBtn'));
 
-function formateDate(i){
+function formateDate(i) {
     const date = new Date(i);
     return `размещено ${date.getDate()}.${date.getMonth()}.${date.getFullYear()} в ${date.getHours()}:${date.getMinutes()}.`
 }

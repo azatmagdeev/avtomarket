1784a68a79a2ba21234e2b1988f068d5a4a969a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-01 13:19:07 +0000

    Deploy azatmagdeev/avtomarket to github.com/azatmagdeev/avtomarket.git:gh-pages

--- js/lib.js
+++ js/lib.js
@@ -1,29 +1,43 @@
-export class Ads{
-    constructor(){
+export class Ads {
+    constructor() {
         this.apiUrl = 'https://avito-back.herokuapp.com/';
         this.items = [];
         this.sellers = [];
     }
 
-    getItems(success){
+    getItems(success) {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET',`${this.apiUrl}`);
-        xhr.addEventListener('load',()=>{
+        xhr.open('GET', `${this.apiUrl}`);
+        xhr.addEventListener('load', () => {
             this.items = JSON.parse(xhr.responseText);
             success(this.items);
         });
         xhr.send();
     }
 
-    getSellers(success){
+    getSellers(success) {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET',`${this.apiUrl}sellers`);
-        xhr.addEventListener('load',()=>{
+        xhr.open('GET', `${this.apiUrl}sellers`);
+        xhr.addEventListener('load', () => {
             this.sellers = JSON.parse(xhr.responseText);
             success(this.sellers);
         });
-        console.log('this.sellers',this.sellers);
+        console.log('this.sellers', this.sellers);
         xhr.send();
+    }
+
+    addNewSeller(name, password, phoneNumber, email, success) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', `${this.apiUrl}sellers`);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.addEventListener('load', () => {
+            success()
+        });
+        const newSeller = {
+            name: name,
+            password: password, phoneNumber: phoneNumber, email: email
+        };
+        xhr.send(newSeller)
     }
 }
 
@@ -33,6 +47,6 @@ export function formateDate(i) {
 }
 
 
-export function loading(root){
+export function loading(root) {
     root.innerHTML = `<img src="img/load.gif" width="100%" alt="Загрузка...">`
 }
